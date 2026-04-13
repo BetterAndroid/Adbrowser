4e24852e16bbbf487a5f8acf397ec4faf18e26e5
fankesyooni <qzmmcn@163.com>
2026-04-13 17:45:00 +0800

    test: move DeviceDebugTest to core:adb for manual device debugging

--- core/adb/src/test/kotlin/com/highcapable/adbrowser/core/adb/test/DeviceDebugTest.kt
+++ core/adb/src/test/kotlin/com/highcapable/adbrowser/core/adb/test/DeviceDebugTest.kt
@@ -1,9 +1,38 @@
-package com.highcapable.adbrowser.app.debug
+/*
+ * Adbrowser - A modern cross-platform Android file manager powered by ADB.
+ * Copyright (C) 2019 HighCapable
+ * https://github.com/BetterAndroid/Adbrowser
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * <p>
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ *
+ * This file is created by fankes on 2026/4/13.
+ */
+package com.highcapable.adbrowser.core.adb.test
 
-import com.highcapable.adbrowser.app.debug.DeviceDebugTest.Companion.ENABLE_REAL_DEVICE_TESTS
+import com.highcapable.adbrowser.core.adb.AdbClient
+import com.highcapable.adbrowser.core.adb.AdbEnvironment
+import com.highcapable.adbrowser.core.adb.di.AdbComponent
+import com.highcapable.adbrowser.core.adb.di.create
+import com.highcapable.adbrowser.core.adb.fs.FileSystemService
 import com.highcapable.adbrowser.core.adb.fs.model.DeviceFileEntry
 import com.highcapable.adbrowser.core.adb.model.AndroidDevice
-import com.highcapable.adbrowser.core.domain.AppServices
+import com.highcapable.adbrowser.core.adb.permission.PermissionService
+import com.highcapable.adbrowser.core.adb.test.DeviceDebugTest.Companion.ENABLE_REAL_DEVICE_TESTS
+import com.highcapable.adbrowser.core.logging.di.LoggingComponent
+import com.highcapable.adbrowser.core.logging.di.create
 import kotlinx.coroutines.runBlocking
 import org.junit.Assume.assumeTrue
 import kotlin.test.Test
@@ -11,11 +40,19 @@ import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
 /**
- * Manual device debug tests for core services from app module.
+ * Manual device debug tests for core adb module.
  *
  * Toggle [ENABLE_REAL_DEVICE_TESTS] to true when you want to run these tests locally.
  */
 class DeviceDebugTest {
+
+    private data class Services(
+        val adbClient: AdbClient,
+        val fileSystemService: FileSystemService,
+        val permissionService: PermissionService,
+        val adbExecPath: String,
+        val useSuperuser: Boolean
+    )
 
     private companion object {
 
@@ -27,7 +64,7 @@ class DeviceDebugTest {
         const val ADB_EXEC_PATH = ""
 
         // Set to true if you want to test with root permissions.
-        const val USE_ROOT = false
+        const val USE_SUPERUSER = false
 
         const val LIST_PATH = "/"
     }
@@ -45,7 +82,7 @@ class DeviceDebugTest {
 
         println("[DEBUG] connected devices (${devices.size}):")
         devices.forEachIndexed { index, device ->
-            println("[DEBUG] #$index ${device.serial} ${device.name} ${device.model} online=${device.isOnline}")
+            println("[DEBUG] #$index $device online=${device.isOnline}")
         }
     }
 
@@ -85,21 +122,29 @@ class DeviceDebugTest {
         println("[DEBUG] permission for '$targetPath': ${info.symbolicPermission} (${info.numericPermission})")
     }
 
-    private suspend fun initializeServices(): AppServices {
-        val services = AppServices()
-        services.initialize()
-
-        services.settingsService.current.adbExecPath = ADB_EXEC_PATH
-        services.settingsService.current.useSuperuser = USE_ROOT
+    private suspend fun initializeServices(): Services {
+        val environment = AdbEnvironment(
+            adbExecPath = { ADB_EXEC_PATH },
+            useSuperuser = { USE_SUPERUSER }
+        )
+        val logging = LoggingComponent::class.create()
+        val adb = AdbComponent::class.create(logging, environment)
+        val services = Services(
+            adbClient = adb.provideAdbClient(),
+            fileSystemService = adb.provideFileSystemService(),
+            permissionService = adb.providePermissionService(),
+            adbExecPath = ADB_EXEC_PATH,
+            useSuperuser = USE_SUPERUSER
+        )
 
         val validateResult = services.adbClient.validateExecPath()
         assertTrue(validateResult.isOk, validateResult.errorMessage ?: "ADB executable path is invalid.")
 
-        println("[DEBUG] useRoot=$USE_ROOT adbPath='${services.settingsService.current.adbExecPath}'")
+        println("[DEBUG] useSuperuser=${services.useSuperuser} adbPath='${services.adbExecPath}'")
         return services
     }
 
-    private suspend fun initializeServicesAndFirstDevice(): Pair<AppServices, AndroidDevice> {
+    private suspend fun initializeServicesAndFirstDevice(): Pair<Services, AndroidDevice> {
         val services = initializeServices()
         val devicesResult = services.adbClient.listDevices()
 
@@ -117,7 +162,7 @@ class DeviceDebugTest {
         )
     }
 
-    private fun buildFullPath(entry: DeviceFileEntry): String = if (entry.path == "/") 
+    private fun buildFullPath(entry: DeviceFileEntry) = if (entry.path == "/")
         "/${entry.name}"
     else "${entry.path.trimEnd('/')}/${entry.name}"
 
